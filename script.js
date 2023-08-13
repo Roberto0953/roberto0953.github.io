0f7389df49e8595f1cfc1d4e51e5bbf52b6e5e9a
Roberto0953 <131179700+Roberto0953@users.noreply.github.com>
2023-08-13 12:32:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,62 @@
+const themeToggler = document.getElementById('theme-toggler');
+const currentTheme = localStorage.getItem('theme');
 
-        const themeToggler = document.getElementById('theme-toggler');
-        const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.body.className = currentTheme;
+} else {
+    document.body.classList.add('night-mode');
+    localStorage.setItem('theme', 'night-mode');
+}
 
-        if (currentTheme) {
-            document.body.className = currentTheme;
-        } else {
-            document.body.classList.add('night-mode');
-            localStorage.setItem('theme', 'night-mode');
-        }
+themeToggler.addEventListener('click', () => {
+    let theme = 'night-mode';
+    if (document.body.classList.contains('night-mode')) {
+        theme = 'light-mode';
+        themeToggler.innerHTML = '<i class="fas fa-moon"></i>';
+    } else {
+        theme = 'night-mode';
+        themeToggler.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+    document.body.className = theme;
+    localStorage.setItem('theme', theme);
+});
 
-        themeToggler.addEventListener('click', () => {
-            let theme = 'night-mode';
-            if (document.body.classList.contains('night-mode')) {
-                theme = 'light-mode';
-                themeToggler.innerHTML = '<i class="fas fa-moon"></i>';
-            } else {
-                theme = 'night-mode';
-                themeToggler.innerHTML = '<i class="fas fa-sun"></i>';
-            }
-            document.body.className = theme;
-            localStorage.setItem('theme', theme);
-        });
+const welcomeTextElement = document.getElementById('welcome-text');
+const languageIconElement = document.getElementById('language-icon');
 
-        const welcomeTextElement = document.getElementById('welcome-text');
-        const languageIconElement = document.getElementById('language-icon');
+let languageIndex = 0;
 
-        let languageIndex = 0;
+const languages = [
+    {
+        text: "Benvenuti nel mio sito web",
+        icon: "flag it"
+    },
+    {
+        text: "Welcome to my website",
+        icon: "flag us"
+    },
+    {
+        text: "Bienvenue sur mon site web",
+        icon: "flag fr"
+    }
+];
 
-        const languages = [
-            {
-                text: "Benvenuti nel mio sito web",
-                icon: "fa-flag"
-            },
-            {
-                text: "Welcome to my website",
-                icon: "fa-flag-usa"
-            },
-            {
-                text: "Bienvenue sur mon site web",
-                icon: "fa-flag-fr"
-            }
-        ];
+function updateLanguage() {
+    welcomeTextElement.classList.add('fade-out');
 
-        function updateLanguage() {
-            welcomeTextElement.classList.add('fade-out');
+    setTimeout(() => {
+        welcomeTextElement.textContent = languages[languageIndex].text;
+        languageIconElement.className = languages[languageIndex].icon;
 
-            setTimeout(() => {
-                welcomeTextElement.textContent = languages[languageIndex].text;
-                languageIconElement.className = `fas ${languages[languageIndex].icon}`;
+        welcomeTextElement.classList.remove('fade-out');
+        welcomeTextElement.classList.add('fade-in');
 
-                welcomeTextElement.classList.remove('fade-out');
-                welcomeTextElement.classList.add('fade-in');
+        setTimeout(() => {
+            welcomeTextElement.classList.remove('fade-in');
+        }, 500);
 
-                setTimeout(() => {
-                    welcomeTextElement.classList.remove('fade-in');
-                }, 500);
+        languageIndex = (languageIndex + 1) % languages.length;
+    }, 500);
+}
 
-                languageIndex = (languageIndex + 1) % languages.length;
-            }, 500);
-        }
-
-        setInterval(updateLanguage, 3000);
+setInterval(updateLanguage, 3000);
